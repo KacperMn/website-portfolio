958483a1a6c2c2d6f67ae1d497bec7f416e30461
Kacper Mnikołajski <kacpermnikolajski@Kacpers-MacBook-Pro.local>
2025-03-18 13:58:17 +0100

    Updated styling

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,15 @@ main = Blueprint('main', __name__)
 @main.route("/")
 def home():
     return render_template("index.html")
+
+@main.route("/about")
+def about():
+    return render_template("about.html")
+
+@main.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@main.route("/contact")
+def contact():
+    return render_template("contact.html")
